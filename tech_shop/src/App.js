84b40a2bdb0ff38ebb9f29d1b3a1dbea9640e00e
SWP 391 <swp391.se1738@gmail.com>
2023-10-24 02:22:46 +0700

    Cart Front-end

--- tech_shop/src/App.js
+++ tech_shop/src/App.js
@@ -1,9 +1,10 @@
+import Cart from "./Customer/Components/Cart";
 import Footer from "./Customer/Components/Footer";
 import Header from "./Customer/Components/Header";
 import ProductDetail from "./Customer/Components/ProductDetail";
 import ProductList from "./Customer/Components/ProductList";
 import ManagerLayout from "./Manager/Components/ManagerLayout";
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/*<main><ProductList /></main> */}
       {/* <ManagerLayout/> */}
 
-      <ProductDetail />
+      {/* <ProductDetail /> */}
+      <Cart/>
       <footer>
         <Footer />
       </footer>
--- tech_shop/src/index.js
+++ tech_shop/src/index.js
@@ -6,6 +6,7 @@ import 'react-bootstrap'
 import 'react-router-dom'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.js';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
